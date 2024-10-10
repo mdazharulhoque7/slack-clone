cfd28fe4e5e0e534272cb3c69cab8228ad409968
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2024-10-10 10:02:07 +0600

    complited user avater menu with logout action

--- src/app/features/auth/components/user-button.tsx
+++ src/app/features/auth/components/user-button.tsx
@@ -12,22 +12,45 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "../hooks/use-current-user";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useRouter } from "next/navigation";
 
 
 export const UserButton = () => {
+    const route = useRouter()
+    const { data, isLoading } = useCurrentUser();
+    const { signOut } = useAuthActions();
+
+    if (isLoading) {
+        return <Loader className="size-4 animate-spin text-muted-foreground" />;
+    }
+    if (!data) return null;
+
+    const { image, name, email } = data;
+    const avaterFallback = name ? name.charAt(0).toUpperCase() : ''
+
+    const handleSignOut = ()=>{
+        signOut().then(()=>{
+          route.push('/auth')
+        })
+    }
+
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative">
                 <Avatar className="size-10">
-                    <AvatarImage/>
-                    <AvatarFallback>
-
+                    <AvatarImage alt={name} src={image} />
+                    <AvatarFallback className="bg-[#5c3b58] text-white">
+                        {avaterFallback}
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="center" side="right" className="w-60">
-                <DropdownMenuItem>
-                    
+                <DropdownMenuItem onClick={() => handleSignOut()} className="h-10">
+                    <LogOut className="size-4 mr-2" />
+                    Log out
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,12 @@
 'use client';
 
-import { useRouter } from 'next/navigation'
-import {useAuthActions} from "@convex-dev/auth/react"
-import { Button } from "@/components/ui/button";
-import { nextjsMiddlewareRedirect } from '@convex-dev/auth/nextjs/server';
 import { UserButton } from './features/auth/components/user-button';
 
 
 export default function Home() {
-  const route = useRouter()
-  const {signOut} = useAuthActions()
-const handleSignOut = ()=>{
-  signOut().then(()=>{
-    route.push('/auth')
-  })
 
-}
   return (
     <div className="flex flex-col items-center w-full h-full justify-center gap-y-5">
-        <p>
-        You are logged in!
-      </p>
       <UserButton/>
     </div>
   );
