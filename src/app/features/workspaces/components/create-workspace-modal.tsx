1aab54acd954f8f53c35f65816e262c256dfa2a9
Md. Azharul Hoque <mdazharulhoque7@gmail.com>
2024-10-11 10:24:59 +0600

    add workspace form

--- src/app/features/workspaces/components/create-workspace-modal.tsx
+++ src/app/features/workspaces/components/create-workspace-modal.tsx
@@ -2,12 +2,14 @@
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 
-export const CreateWorkspaceModal = ()=>{
+export const CreateWorkspaceModal = () => {
     const [open, setOpen] = useCreateWorkspaceModal();
 
-    const handleClose = ()=> {
+    const handleClose = () => {
         setOpen(false);
         // TODO: Clear Form
     }
@@ -15,9 +17,23 @@ export const CreateWorkspaceModal = ()=>{
     return (
         <Dialog onOpenChange={handleClose} open={open}>
             <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Add a workspace</DialogTitle>
-            </DialogHeader>
+                <DialogHeader>
+                    <DialogTitle>Add a workspace</DialogTitle>
+                </DialogHeader>
+                <form action="" className="space-y-4">
+                    <Input
+                        value=""
+                        disabled={false}
+                        required
+                        autoFocus
+                        minLength={3}
+                        placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                    />
+                    <div className="flex justify-end">
+
+                        <Button disabled={false}>Create</Button>
+                    </div>
+                </form>
             </DialogContent>
         </Dialog>
     )
