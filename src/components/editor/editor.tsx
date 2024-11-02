6c4aa81546d4d7fd36c8199b1891984429a0bed4
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2024-11-02 09:37:27 +0600

    image picker completed ts-part2-00:22:30

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -3,12 +3,13 @@ import "quill/dist/quill.snow.css"
 import { MutableRefObject, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { Button } from '../ui/button';
 import { PiTextAa } from 'react-icons/pi'
-import { ImageIcon, Smile } from 'lucide-react';
+import { ImageIcon, Smile, XIcon } from 'lucide-react';
 import { MdSend } from 'react-icons/md';
 import { ToolTip } from '../ui/custom/tooltip';
 import { Delta, Op } from 'quill/core';
 import { cn } from '@/lib/utils';
 import EmojiPopover from './emoji-popover';
+import Image from 'next/image';
 
 
 type EditorValue = {
@@ -35,8 +36,11 @@ const Editor = ({
     innerRef,
     variant = "create"
 }: editorProps) => {
-    const [text, setText] = useState("")
+    const [text, setText] = useState("");
+    const [image, setImage] = useState<File | null>(null);
     const [toolbarVisible, setToolbarVisible] = useState<boolean>(true)
+
+
     const containerRef = useRef<HTMLDivElement>(null);
     const quillRef = useRef<Quill | null>(null);
     const submitRef = useRef(onSubmit);
@@ -44,6 +48,7 @@ const Editor = ({
     const placeholderRef = useRef(placeholder);
     const defaultValueRef = useRef(defaultValue);
     const disabledRef = useRef(disabled);
+    const imageElementRef = useRef<HTMLInputElement>(null);
 
     useLayoutEffect(() => {
         submitRef.current = onSubmit;
@@ -66,32 +71,32 @@ const Editor = ({
         const editorOption: QuillOptions = {
             theme: "snow",
             placeholder: placeholderRef.current,
-            modules:{
-                toolbar:[
+            modules: {
+                toolbar: [
                     ["bold", "italic", "strike"],
                     ["link"],
-                    [{list: "ordered"}, {list: "bullet"}]
+                    [{ list: "ordered" }, { list: "bullet" }]
                 ],
-                keyboard:{
+                keyboard: {
 
-                    bindings:{
+                    bindings: {
                         enter: {
                             key: "Enter",
-                            handler: ()=>{
+                            handler: () => {
                                 // TODO Submit form
                                 return;
                             }
                         },
-                        shift_enter:{
+                        shift_enter: {
                             key: "Enter",
                             shiftKey: true,
-                            handler: ()=>{
+                            handler: () => {
                                 quill.insertText(quill.getSelection()?.index || 0, "\n")
                             }
                         }
 
                     }
-                } 
+                }
             }
         }
 
@@ -99,7 +104,7 @@ const Editor = ({
         quillRef.current = quill
         quillRef.current.focus()
 
-        if(innerRef){
+        if (innerRef) {
             innerRef.current = quill;
         }
 
@@ -107,7 +112,7 @@ const Editor = ({
         setText(quill.getText())
 
         // Listen to quill text change event and update text state accordingly
-        quill.on(Quill.events.TEXT_CHANGE, ()=>{
+        quill.on(Quill.events.TEXT_CHANGE, () => {
             setText(quill.getText())
         })
 
@@ -119,29 +124,37 @@ const Editor = ({
             if (container) {
                 container.innerHTML = ""
             }
-            if(quillRef.current){
+            if (quillRef.current) {
                 quillRef.current = null;
             }
-            if(innerRef){
+            if (innerRef) {
                 innerRef.current = null;
             }
         }
     }, [innerRef])
 
     const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
-    const toggleToolbar = ()=>{
-        setToolbarVisible((value)=> !value)    
+    const toggleToolbar = () => {
+        setToolbarVisible((value) => !value)
         const toolbarElement = containerRef.current?.querySelector('.ql-toolbar');
-        if(toolbarElement){
+        if (toolbarElement) {
             toolbarElement.classList.toggle('hidden')
         }
     }
-    const onEmojiSelect = (emoji: any) => { 
+    const onEmojiSelect = (emoji: any) => {
         const quill = quillRef.current;
         quill?.insertText(quill?.getSelection()?.index || 0, emoji.native)
     }
     return (
         <div className='flex flex-col'>
+            <input
+                type="file"
+                accept='image/*'
+                ref={imageElementRef}
+                onChange={(e) => setImage(e.target.files![0])}
+                className='hidden'
+
+            />
             <div
                 className='flex flex-col border border-slate-200 rounded-md
                         overflow-hidden focus-within:border-slate-300
@@ -150,8 +163,33 @@ const Editor = ({
             >
 
                 <div ref={containerRef} className='h-full ql-custom' />
+                {!!image && (
+                    <div className="p-2">
+                        <div className="relative size-[62px] flex items-center justify-center group/image">
+                        <ToolTip label='Remove image'>
+                        <button
+                            onClick={()=>{
+                                setImage(null);
+                                imageElementRef.current!.value = ""
+                            }}
+                            className='hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black
+                                        absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2
+                                         border-white items-center justify-center'
+                        >
+                            <XIcon className='size-3.5' />
+                        </button>
+                        </ToolTip>
+                        <Image
+                            alt='Uploaded'
+                            src={URL.createObjectURL(image)}
+                            fill
+                            className='rounded-xl overflow-hidden border object-cover'
+                        />
+                        </div>
+                    </div>
+                )}
                 <div className="flex px-2 pb-2 z-[5]">
-                    <ToolTip label={toolbarVisible ? 'Hide formatting': 'Show formatting'}>
+                    <ToolTip label={toolbarVisible ? 'Hide formatting' : 'Show formatting'}>
                         <Button
                             variant="ghost"
                             size="iconSm"
@@ -177,7 +215,7 @@ const Editor = ({
                                     variant="ghost"
                                     size="iconSm"
                                     disabled={disabled}
-                                    onClick={() => { }}
+                                    onClick={() => imageElementRef.current?.click()}
                                 >
                                     <ImageIcon className='size-4' />
                                 </Button>
@@ -188,12 +226,12 @@ const Editor = ({
                                 disabled={disabled || isEmpty}
                                 onClick={() => { }}
                                 className={
-                                    cn('ml-auto', 
-                                        isEmpty ? 
-                                        'bg-white hover:bg-white text-muted-foreground'
-                                        : 'text-white hover:text-white bg-[#007a5a] hover:bg-[#007a5a]/80'
+                                    cn('ml-auto',
+                                        isEmpty ?
+                                            'bg-white hover:bg-white text-muted-foreground'
+                                            : 'text-white hover:text-white bg-[#007a5a] hover:bg-[#007a5a]/80'
                                     )
-                                    }
+                                }
                             >
                                 <MdSend className='size-4' />
                             </Button>
