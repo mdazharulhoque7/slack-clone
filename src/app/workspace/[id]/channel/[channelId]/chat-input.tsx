c4c83d3cbb52cbf700234b5355b8d09d5626d393
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2024-11-02 11:52:47 +0600

    completed message create api

--- src/app/workspace/[id]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[id]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,9 @@
 import { useRef } from "react"
 import dynamic from "next/dynamic"
 import Quill from "quill"
+import { useCreateMessage } from "@/app/features/messages/api/use-create-message"
+import { useWorkspaceId } from "@/hooks/use-workspace_id"
+import { useChannelId } from "@/hooks/use-channel_id"
 
 const Editor = dynamic(()=> import ("@/components/editor/editor"),{ssr: false})
 
@@ -10,6 +13,9 @@ interface ChatInputProps {
 
 const ChatInput = ({placeholder}:ChatInputProps) => {
   const editorRef = useRef<Quill|null>(null);
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+  const {mutate: createMessage, isPending:createMessagePending} = useCreateMessage();
 
   const handleSubmit = (
     {body, 
@@ -20,6 +26,11 @@ const ChatInput = ({placeholder}:ChatInputProps) => {
     }
   )=>{
     console.log(body, image)
+    createMessage({
+      workspaceId,
+      channelId,
+      body
+    })
   }
   return (
     <div className="px-5 w-full">
